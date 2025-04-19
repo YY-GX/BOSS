1b1edb22286f8e5a3b91c105b27a0276336c3a8a
yy-gx <yyang9923@gmail.com>
2025-04-19 00:17:26 -0400

    add r3m to BC-Transformer

--- libero/lifelong/models/bc_transformer_policy.py
+++ libero/lifelong/models/bc_transformer_policy.py
@@ -195,6 +195,8 @@ class ExtraModalityTokens(nn.Module):
             (self.use_gripper, "gripper_states"),
             (self.use_ee, "ee_states"),
         ]:
+            print(f"[debug] {modality_name} device: {obs_dict[modality_name].device}")
+            print(f"[debug] encoder device: {next(self.extra_encoders[modality_name]['encoder'].parameters()).device}")
             if use_modality:
                 encoded = self.extra_encoders[modality_name](obs_dict[modality_name])
                 tensor_list.append(encoded)
