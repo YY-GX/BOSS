4711c021bdee46f52a39191a867c4b7e841deac1
yy-gx <yyang9923@gmail.com>
2025-04-19 00:09:15 -0400

    add r3m to BC-Transformer

--- libero/lifelong/models/bc_transformer_policy.py
+++ libero/lifelong/models/bc_transformer_policy.py
@@ -78,9 +78,10 @@ class ExtraModalityTokens(nn.Module):
             if use_modality:
                 generate_proprio_mlp_fn(modality_name, proprio_dim)
 
-        self.encoders = nn.ModuleList(
-            [x["encoder"] for x in self.extra_encoders.values()]
-        )
+        # self.encoders = nn.ModuleList(
+        #     [x["encoder"] for x in self.extra_encoders.values()]
+        # )
+        self.encoders = nn.ModuleList(list(self.extra_encoders.values()))
 
     def forward(self, obs_dict):
         """
