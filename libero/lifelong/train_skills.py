6322c05d4dd12baf2709a8f3d0dd97f61c728fd6
yy-gx <yyang9923@gmail.com>
2025-04-20 23:19:26 -0400

    add liv to BC-Transformer

--- libero/lifelong/train_skills.py
+++ libero/lifelong/train_skills.py
@@ -75,22 +75,31 @@ def main(hydra_cfg):
     descriptions = []
     shape_meta = None
     for i in range(n_manip_tasks):
-        # currently we assume tasks from same benchmark have the same shape_meta
-        try:
-            task_i_dataset, shape_meta = get_dataset(
-                dataset_path=os.path.join(
-                    cfg.folder, benchmark.get_task_demonstration(i)
-                ),
-                obs_modality=cfg.data.obs.modality,
-                initialize_obs_utils=True,
-                seq_len=cfg.data.seq_len,
-            )
-        except Exception as e:
-            print(
-                f"[error] failed to load task {i} name {benchmark.get_task_names()[i]}"
-            )
-            print(f"[error] {e}")
-            exit(1)
+        task_i_dataset, shape_meta = get_dataset(
+            dataset_path=os.path.join(
+                cfg.folder, benchmark.get_task_demonstration(i)
+            ),
+            obs_modality=cfg.data.obs.modality,
+            initialize_obs_utils=True,
+            seq_len=cfg.data.seq_len,
+        )
+
+        # # currently we assume tasks from same benchmark have the same shape_meta
+        # try:
+        #     task_i_dataset, shape_meta = get_dataset(
+        #         dataset_path=os.path.join(
+        #             cfg.folder, benchmark.get_task_demonstration(i)
+        #         ),
+        #         obs_modality=cfg.data.obs.modality,
+        #         initialize_obs_utils=True,
+        #         seq_len=cfg.data.seq_len,
+        #     )
+        # except Exception as e:
+        #     print(
+        #         f"[error] failed to load task {i} name {benchmark.get_task_names()[i]}"
+        #     )
+        #     print(f"[error] {e}")
+        #     exit(1)
         # add language to the vision dataset, hence we call vl_dataset
         task_description = benchmark.get_task(i).language
         # maintain a list containing (lang, ds)
