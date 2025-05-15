e2c6b97c7f05a53f572f4540258c061f5d39b091
YY-GX <yyang9923@gmail.com>
2025-05-14 20:51:26 -0400

    add smaller/larger objects

--- scripts/generate_inits.py
+++ scripts/generate_inits.py
@@ -3,28 +3,31 @@ from libero.libero.envs import OffScreenRenderEnv
 import os
 
 def create_init(bddl_file_name, suite_name):
+    N_init, N_pruned_init = 50, 20
     out_folder = f"/mnt/arc/yygx/paper_codebases/RA-L_25/BOSS/libero/libero/init_files/{suite_name}"
     os.makedirs(out_folder, exist_ok=True)
-    config_dict = {
-        ".init": 50,
-        ".pruned_init": 20,
-    }
-    for suffix, N in config_dict.items():
-        out_file = os.path.join(out_folder, bddl_file_name.split(suite_name)[-1].replace(".bddl", suffix).lstrip('/'))
-        env_args = {
-                    "bddl_file_name": bddl_file_name,
-                    "camera_heights": 128,
-                    "camera_widths": 128,
-                }
-        env = OffScreenRenderEnv(**env_args)
-        dim = env.get_sim_state().shape[0]
-        env.reset()
-        init_states = torch.from_numpy(env.get_sim_state().reshape((1, dim)))
-        for _ in range(N - 1):
-            env.reset()
-            init_states = torch.vstack([init_states, torch.from_numpy(env.get_sim_state().reshape((1, dim)))])
-        torch.save(init_states, out_file)
 
+    # First, generate the full 50 samples
+    base_file = os.path.join(out_folder, bddl_file_name.split(suite_name)[-1].replace(".bddl", ".init").lstrip('/'))
+    env_args = {
+        "bddl_file_name": bddl_file_name,
+        "camera_heights": 128,
+        "camera_widths": 128,
+    }
+    env = OffScreenRenderEnv(**env_args)
+    dim = env.get_sim_state().shape[0]
+    env.reset()
+    init_states = torch.from_numpy(env.get_sim_state().reshape((1, dim)))
+    for _ in range(N_init):
+        env.reset()
+        init_states = torch.vstack([init_states, torch.from_numpy(env.get_sim_state().reshape((1, dim)))])
+    torch.save(init_states, base_file)
+
+    # Sample 20 from the 50 to create .pruned_init
+    pruned_file = base_file.replace(".init", ".pruned_init")
+    indices = random.sample(range(N_init), N_pruned_init)
+    pruned_states = init_states[indices]
+    torch.save(pruned_states, pruned_file)
 
 suite_name = "gl_size"
 folder = f"/mnt/arc/yygx/paper_codebases/RA-L_25/BOSS/libero/libero/bddl_files/{suite_name}"
